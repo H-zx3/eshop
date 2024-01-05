8942fa6711ec3ebaa5d486ed0404a93c4ff5c504
H-zx3 <132924767+H-zx3@users.noreply.github.com>
2024-01-05 20:17:28 +0200

    Update settings.py

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -152,5 +152,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'contact.rytis@gmail.com'
-EMAIL_HOST_PASSWORD = 'nlzu uvtz ruof ujhi'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
