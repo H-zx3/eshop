af3f4886a6c73ef434885077a79aabb980f7af0a
H-zx3 <s.rytis@yahoo.com>
2024-01-05 19:05:59 +0200

    django authentication working

--- greatkart/urls.py
+++ greatkart/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('carts.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT
            )
 if settings.DEBUG:
